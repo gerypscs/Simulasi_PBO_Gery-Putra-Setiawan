eee4367bc7a962ffaad2ef48c3897a55746c7b55
Gery Putra Setiawan <59498820+gerypscs@users.noreply.github.com>
2026-06-17 11:46:57 +0700

    [Tahap 3] Membuat Koneksi Database

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 $host     = "localhost";
 $username = "root";
 $password = "";
-$database = "simulasi_pbo_ti-1c_gery putra setiawan"; // Pastikan nama DB sesuai dengan yang Anda buat di phpMyAdmin
+$database = "db_simulasi_pbo_ti-1c_gery_putra-setiawan"; // Pastikan nama DB sesuai dengan yang Anda buat di phpMyAdmin
 
 // Membuat koneksi
 $koneksi = mysqli_connect($host, $username, $password, $database);
