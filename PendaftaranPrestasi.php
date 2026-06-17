474c23751f437ddaaa7cb9eb86d212eb1b6ee66b
Gery Putra Setiawan <59498820+gerypscs@users.noreply.github.com>
2026-06-17 11:04:23 +0700

    [Tahap 4 & 5] Implementasi Pewarisan (Inheritance) & Metode Query Spesifik dan Implementasi Polimorfisme (Polymorphism Overriding)

--- PendaftaranPrestasi.php
+++ PendaftaranPrestasi.php
@@ -1,21 +1,25 @@
 <?php
 require_once 'Pendaftaran.php';
+require_once 'koneksi.php';
 
 class PendaftaranPrestasi extends Pendaftaran {
+    // Properti tambahan spesifik kelas anak (Tahap 4)
     private $jenis_prestasi;
     private $tingkat_prestasi;
 
+    // Constructor Kelas Anak
     public function __construct($id_pendaftaran, $nama_calon, $asal_sekolah, $nilai_ujian, $biayaPendaftaranDasar, $jenis_prestasi, $tingkat_prestasi) {
         parent::__construct($id_pendaftaran, $nama_calon, $asal_sekolah, $nilai_ujian, $biayaPendaftaranDasar);
         $this->jenis_prestasi = $jenis_prestasi;
         $this->tingkat_prestasi = $tingkat_prestasi;
     }
 
-    // Method Overriding Perhitungan Biaya dengan Potongan Prestasi
+    // Tahap 5: Method Overriding Perhitungan Biaya Jalur Prestasi (Potongan Rp50.000)
     public function hitungTotalBiaya() {
         return $this->biayaPendaftaranDasar - 50000;
     }
 
+    // Implementasi menampilkan info spesifik (Tahap 3 & 4)
     public function tampilkanInfoSpesifik() {
         echo "=== FASILITAS JALUR PRESTASI ===<br>";
         echo "Jenis Prestasi: " . $this->jenis_prestasi . "<br>";
@@ -23,17 +27,22 @@ class PendaftaranPrestasi extends Pendaftaran {
         echo "---------------------------------<br>";
     }
 
-    // Metode Query database untuk menarik data Jalur Prestasi
+    // Metode Tambahan: Mengambil data khusus jalur Prestasi dari database
     public static function ambilDataPrestasi($koneksi) {
         $daftar = [];
         $query = "SELECT * FROM tabel_pendaftaran WHERE jalur_pendaftaran = 'Prestasi'";
         $result = mysqli_query($koneksi, $query);
+        
         if ($result) {
             while ($row = mysqli_fetch_assoc($result)) {
                 $daftar[] = new PendaftaranPrestasi(
-                    $row['id_pendaftaran'], $row['nama_calon'], $row['asal_sekolah'],
-                    $row['nilai_ujian'], $row['biaya_pendaftaran_dasar'],
-                    $row['jenis_prestasi'], $row['tingkat_prestasi']
+                    $row['id_pendaftaran'],
+                    $row['nama_calon'],
+                    $row['asal_sekolah'],
+                    $row['nilai_ujian'],
+                    $row['biaya_pendaftaran_dasar'],
+                    $row['jenis_prestasi'],
+                    $row['tingkat_prestasi']
                 );
             }
         }
